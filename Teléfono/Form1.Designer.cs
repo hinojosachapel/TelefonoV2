47d84688e128178df31909dbd03023ddcf398e58
Rubén Hinojosa Chapel <hinojo_sa@hotmail.com>
2016-03-10 18:58:51 +0100

    First commit

---
+++ Teléfono/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace RH.MobilePhone
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MainMenu mainMenu1;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenu1 = new System.Windows.Forms.MainMenu();
+            this.menuItem1 = new System.Windows.Forms.MenuItem();
+            this.txtBoxPhoneNumber = new System.Windows.Forms.TextBox();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btnCall = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnInicio = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // mainMenu1
+            // 
+            this.mainMenu1.MenuItems.Add(this.menuItem1);
+            // 
+            // menuItem1
+            // 
+            this.menuItem1.Text = "Cerrar";
+            this.menuItem1.Click += new System.EventHandler(this.menuItem1_Click);
+            // 
+            // txtBoxPhoneNumber
+            // 
+            this.txtBoxPhoneNumber.Location = new System.Drawing.Point(27, 41);
+            this.txtBoxPhoneNumber.MaxLength = 15;
+            this.txtBoxPhoneNumber.Name = "txtBoxPhoneNumber";
+            this.txtBoxPhoneNumber.Size = new System.Drawing.Size(145, 21);
+            this.txtBoxPhoneNumber.TabIndex = 2;
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(178, 41);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(36, 20);
+            this.btnDel.TabIndex = 3;
+            this.btnDel.Text = "<-";
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Regular);
+            this.btn1.Location = new System.Drawing.Point(27, 81);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(55, 30);
+            this.btn1.TabIndex = 4;
+            this.btn1.Text = "1";
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Regular);
+            this.btn2.Location = new System.Drawing.Point(93, 81);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(55, 30);
+            this.btn2.TabIndex = 5;
+            this.btn2.Text = "2";
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Regular);
+            this.btn3.Location = new System.Drawing.Point(159, 81);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(55, 30);
+            this.btn3.TabIndex = 6;
+            this.btn3.Text = "3";
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Regular);
+            this.btn4.Location = new System.Drawing.Point(27, 126);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(55, 30);
+            this.btn4.TabIndex = 7;
+            this.btn4.Text = "4";
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Regular);
+            this.btn5.Location = new System.Drawing.Point(93, 126);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(55, 30);
+            this.btn5.TabIndex = 8;
+            this.btn5.Text = "5";
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Regular);
+            this.btn6.Location = new System.Drawing.Point(159, 126);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(55, 30);
+            this.btn6.TabIndex = 9;
+            this.btn6.Text = "6";
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Regular);
+            this.btn7.Location = new System.Drawing.Point(27, 172);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(55, 30);
+            this.btn7.TabIndex = 10;
+            this.btn7.Text = "7";
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Regular);
+            this.btn8.Location = new System.Drawing.Point(93, 172);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(55, 30);
+            this.btn8.TabIndex = 11;
+            this.btn8.Text = "8";
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Regular);
+            this.btn9.Location = new System.Drawing.Point(159, 172);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(55, 30);
+            this.btn9.TabIndex = 12;
+            this.btn9.Text = "9";
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Regular);
+            this.btn0.Location = new System.Drawing.Point(27, 217);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(55, 30);
+            this.btn0.TabIndex = 13;
+            this.btn0.Text = "0";
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btnCall
+            // 
+            this.btnCall.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Regular);
+            this.btnCall.Location = new System.Drawing.Point(93, 217);
+            this.btnCall.Name = "btnCall";
+            this.btnCall.Size = new System.Drawing.Size(121, 30);
+            this.btnCall.TabIndex = 14;
+            this.btnCall.Text = "Llamar";
+            this.btnCall.Click += new System.EventHandler(this.btnCall_Click);
+            // 
+            // label1
+            // 
+            this.label1.ForeColor = System.Drawing.Color.Gray;
+            this.label1.Location = new System.Drawing.Point(64, 258);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 20);
+            this.label1.Text = "hinojosachapel.com";
+            // 
+            // btnInicio
+            // 
+            this.btnInicio.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+            this.btnInicio.Location = new System.Drawing.Point(3, 3);
+            this.btnInicio.Name = "btnInicio";
+            this.btnInicio.Size = new System.Drawing.Size(50, 18);
+            this.btnInicio.TabIndex = 16;
+            this.btnInicio.Text = "Inicio";
+            this.btnInicio.Click += new System.EventHandler(this.btnInicio_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(240, 294);
+            this.Controls.Add(this.btnInicio);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCall);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.txtBoxPhoneNumber);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Menu = this.mainMenu1;
+            this.Name = "Form1";
+            this.Text = "Teléfono";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxPhoneNumber;
+        private System.Windows.Forms.MenuItem menuItem1;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btnCall;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnInicio;
+    }
+}
+
---
+++ Teléfono/Form1.cs
@@ -0,0 +1,172 @@
+﻿// Cómo hacer una llamada telefónica en Windows Mobile con C#
+// Cómo abrir el menú Inicio en Windows Mobile con C#
+
+#region License
+
+/* Copyright (c) 2011 Rubén Hinojosa Chapel
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ * of this software and associated documentation files (the "Software"), to 
+ * deal in the Software without restriction, including without limitation the 
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+ * sell copies of the Software, and to permit persons to whom the Software is 
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software. 
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+ * THE SOFTWARE.
+ */
+
+#endregion
+
+#region Contact
+
+/*
+ * Rubén Hinojosa Chapel
+ * Web: www.hinojosachapel.com
+ */
+
+#endregion
+
+#region Using directives
+
+using System;
+using System.Windows.Forms;
+using Microsoft.WindowsMobile.Telephony;
+
+#endregion
+
+namespace RH.MobilePhone
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void menuItem1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            if (txtBoxPhoneNumber.Text.Length > 0)
+            {
+                txtBoxPhoneNumber.Text =
+                    txtBoxPhoneNumber.Text.Substring(0, txtBoxPhoneNumber.Text.Length - 1);
+                txtBoxPhoneNumber.Select(txtBoxPhoneNumber.TextLength, 1);
+                txtBoxPhoneNumber.Focus();
+            }
+        }
+
+        private void AddDigit(char digit)
+        {
+            if (txtBoxPhoneNumber.TextLength < txtBoxPhoneNumber.MaxLength)
+            {
+                txtBoxPhoneNumber.Text += digit;
+                txtBoxPhoneNumber.Select(txtBoxPhoneNumber.TextLength, 1);
+                txtBoxPhoneNumber.Focus();
+            }
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            AddDigit('1');
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            AddDigit('2');
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            AddDigit('3');
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            AddDigit('4');
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            AddDigit('5');
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            AddDigit('6');
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            AddDigit('7');
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            AddDigit('8');
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            AddDigit('9');
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            AddDigit('0');
+        }
+
+        /// <summary>
+        /// Ejecuta una llamada a un número de teléfono
+        /// </summary>
+        private void btnCall_Click(object sender, EventArgs e)
+        {
+            if (txtBoxPhoneNumber.Text == String.Empty)
+            {
+                MessageBox.Show("No existe ningún número de teléfono al cual llamar",
+                    "Información...", MessageBoxButtons.OK, MessageBoxIcon.Asterisk, MessageBoxDefaultButton.Button1);
+                return;
+            }
+
+            Phone p = new Phone();
+            p.Talk(txtBoxPhoneNumber.Text);
+        }
+
+        #region Abrir menú Inicio
+
+        [System.Runtime.InteropServices.DllImport("coredll.dll", EntryPoint = "keybd_event", SetLastError = true)]
+        internal static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+
+        const int VK_LWIN = 0x5B;
+        const int KEYEVENTF_KEYUP = 0x2;
+        const int KEYEVENTF_KEYDOWN = 0x0;
+        
+        /// <summary>
+        /// Abre el menú Inicio
+        /// </summary>
+        private void btnInicio_Click(object sender, EventArgs e)
+        {
+            keybd_event(VK_LWIN, 0, KEYEVENTF_KEYDOWN, 0);
+            keybd_event(VK_LWIN, 0, KEYEVENTF_KEYUP, 0);
+
+            //Código alternativo
+            //keybd_event((byte)Keys.LWin, 0, KEYEVENTF_KEYDOWN, 0);
+            //keybd_event((byte)Keys.LWin, 0, KEYEVENTF_KEYUP, 0);
+        }
+
+        #endregion
+
+    }
+}
